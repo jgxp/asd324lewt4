6e5feb2a358c64e499bbfe434124921f9c06aa22
jgxp <joshfindsdiamondback@gmail.com>
2025-03-17 16:56:44 -0600

    Update asdasdwjkerwerknwerertlkerntdfgdflgknmdlfgmk.lua

--- asdasdwjkerwerknwerertlkerntdfgdflgknmdlfgmk.lua
+++ asdasdwjkerwerknwerertlkerntdfgdflgknmdlfgmk.lua
@@ -1,6 +1,6 @@
 shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​u​t​f​i​t​D​i​s​p​a​t​c​h​e​s​I​X​P​2"] = {
     ["Main"] = {
-        ['Loader'] = { ['Key'] = ('qdXsKBffqasVLrOO5qWMiBRnIrGPF9Y6') },
+        ['Loader'] = { ['Key'] = ('1QzhEFkoMipfMbB6hDhy2q8GYuEqoYDr') },
     },
     
     ["Universal"] = {
